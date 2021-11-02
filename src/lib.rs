f2e19a872a0a00336d3b92371125cb5410dfcdc7
DiscreteTom <discrete_tom@outlook.com>
2021-11-02 16:18:22 +0000

    optimize comments & doc test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 /// **C**losure which will **C**lone its environment.
 ///
-/// ```
+/// # Getting Started
+///
+/// ```ignore
 /// cc!(|@a, @mut b, c| { a + b + c })
 /// ```
 ///
 /// will be translated to:
 ///
-/// ```
+/// ```ignore
 /// {
 ///   let a = a.clone();
 ///   let mut b = b.clone();
@@ -17,59 +19,63 @@
 /// # Examples
 ///
 /// ```
-/// // clone one var
-/// let s1 = String::from("111");
-/// assert_eq!(
-///   cc!(move |@s1| format!("{}", s1))(),
-///   "111"
-/// );
+/// use clonesure::cc;
 ///
-/// // clone many vars
-/// let s1 = String::from("111");
-/// let s2 = String::from("222");
-/// assert_eq!(
-///   cc!(move |@s1, @s2| format!("{} {}", s1, s2))(),
-///   "111 222"
-/// );
-///
-/// // clone var mut
-/// let s1 = String::from("111");
-/// let s2 = String::from("222");
-/// assert_eq!(
-///   cc!(move |@mut s1, @s2| {
-///     s1 = s1 + &s2;
-///     format!("{}", s1)
-///   })(),
-///   "111222"
-/// );
-///
-/// // with closure params
-/// let s1 = String::from("111");
-/// let s2 = String::from("222");
-/// assert_eq!(
-///   cc!(move |@mut s1, @s2, s3| {
-///     s1 = s1 + &s2;
-///     format!("{} {}", s1, s3)
-///   })("333"),
-///   "111222 333"
-/// );
-///
-/// // order & count doesn't matter
-/// let s2 = String::from("222");
-/// let s3 = String::from("333");
-/// let s4 = String::from("444");
-/// let s5 = String::from("555");
-/// assert_eq!(
-///   cc!(move |s1, @s2, @mut s3, @mut s4, @s5, s6| {
-///     s3 = s2 + &s3;
-///     s4 = s4 + &s5;
-///     format!("{} {} {} {}", s1, s3, s4, s6)
-///   })("111", "666"),
-///   "111 222333 444555 666"
-/// );
-///
-/// // ref params are not supported for now, but will be added in the future
-/// // cc!(|&s1, &mut s2| {})
+/// fn main() {
+///   // clone one var
+///   let s1 = String::from("111");
+///   assert_eq!(
+///     cc!(move |@s1| format!("{}", s1))(),
+///     "111"
+///   );
+///  
+///   // clone many vars
+///   let s1 = String::from("111");
+///   let s2 = String::from("222");
+///   assert_eq!(
+///     cc!(move |@s1, @s2| format!("{} {}", s1, s2))(),
+///     "111 222"
+///   );
+///  
+///   // clone var mut
+///   let s1 = String::from("111");
+///   let s2 = String::from("222");
+///   assert_eq!(
+///     cc!(move |@mut s1, @s2| {
+///       s1 = s1 + &s2;
+///       format!("{}", s1)
+///     })(),
+///     "111222"
+///   );
+///  
+///   // with closure params
+///   let s1 = String::from("111");
+///   let s2 = String::from("222");
+///   assert_eq!(
+///     cc!(move |@mut s1, @s2, s3| {
+///       s1 = s1 + &s2;
+///       format!("{} {}", s1, s3)
+///     })("333"),
+///     "111222 333"
+///   );
+///  
+///   // order & count doesn't matter
+///   let s2 = String::from("222");
+///   let s3 = String::from("333");
+///   let s4 = String::from("444");
+///   let s5 = String::from("555");
+///   assert_eq!(
+///     cc!(move |s1, @s2, @mut s3, @mut s4, @s5, s6| {
+///       s3 = s2 + &s3;
+///       s4 = s4 + &s5;
+///       format!("{} {} {} {}", s1, s3, s4, s6)
+///     })("111", "666"),
+///     "111 222333 444555 666"
+///   );
+///  
+///   // ref params are not supported for now, but will be added in the future
+///   // cc!(|&s1, &mut s2| {})
+/// }
 /// ```
 #[macro_export]
 macro_rules! cc {
